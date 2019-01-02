dc2868a4c759a96cc38bbe2d268c2dfd6f40027e
Martin Mihálek <aenniw@gmail.com>
2019-01-02 13:29:30 +0100

    Add speed controll for bridge motor
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- skarsta/lib/motor-bridge/MotorBridge.h
+++ skarsta/lib/motor-bridge/MotorBridge.h
@@ -4,9 +4,12 @@
 #include <Arduino.h>
 #include <Motor.h>
 
+#define INITIAL_SPEED 100
+#define PWM_FREQ_DIV 1024
+
 class MotorBridge : public Motor {
 private:
-    uint8_t r_pwm = 0, l_pwm = 0, r_enable = 0, l_enable = 0;
+    uint8_t r_pwm = 0, l_pwm = 0, r_enable = 0, l_enable = 0, speed;
 protected:
 
     void _off() override;
@@ -15,10 +18,7 @@ protected:
 
     void _dir_ccw() override;
 
-    void setSpeed(uint8_t speed) {
-        digitalWrite(r_pwm, speed);
-        digitalWrite(l_pwm, speed);
-    }
+    void setSpeed(uint8_t speed);
 
 public:
     MotorBridge(uint8_t _pin1, uint8_t _pin2,
@@ -26,9 +26,14 @@ public:
                 uint8_t _pin5, uint8_t _pin6) : Motor(_pin1, _pin2) {
         initPin((r_enable = _pin3));
         initPin((l_enable = _pin4));
+
         initPin((r_pwm = _pin5));
-        initPin((r_pwm = _pin6));
+        setPwmFrequency(r_pwm, PWM_FREQ_DIV);
+        initPin((l_pwm = _pin6));
+        setPwmFrequency(l_pwm, PWM_FREQ_DIV);
     };
+
+    void cycle() override;
 };
 
 #endif //ARDUINO_PROJECTS_ROOT_MOTOR_BRIDGE_H
