215741dc3b65a855f08561c1275d0ec022378329
Martin Mihálek <aenniw@gmail.com>
2019-01-22 22:24:30 +0100

    Mark vars updated in ISR as volatile
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- skarsta/lib/motor-bridge/MotorBridge.cpp
+++ skarsta/lib/motor-bridge/MotorBridge.cpp
@@ -1,5 +1,14 @@
 #include "MotorBridge.h"
 
+MotorBridge::MotorBridge(uint8_t _pin1, uint8_t _pin2, uint8_t _pin3, uint8_t _pin4, uint8_t _pin5, uint8_t _pin6)
+        : Motor(_pin1, _pin2) {
+    initPin((r_enable = _pin3));
+    initPin((l_enable = _pin4));
+    initPin((r_pwm = _pin5));
+    initPin((l_pwm = _pin6));
+    this->enable();
+}
+
 void MotorBridge::_off() {
     digitalWrite(r_enable, LOW);
     digitalWrite(l_enable, LOW);
@@ -37,10 +46,10 @@ void MotorBridge::setSpeed(MotorState state, uint8_t speed) {
 
     if (state == CCW) {
         analogWrite(l_pwm, 0);
-        analogWrite(r_pwm, this->speed);
+        analogWrite(r_pwm, speed);
     } else if (state == CW) {
         analogWrite(r_pwm, 0);
-        analogWrite(l_pwm, this->speed);
+        analogWrite(l_pwm, speed);
     }
 }
 
