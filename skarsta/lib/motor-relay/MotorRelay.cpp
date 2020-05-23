dc58b4f6edca5ed578a42de776d5a7945808af28
Martin Mihalek <aenniw@gmail.com>
2020-05-23 18:10:06 +0200

    Add skarsta inverse relay target
    - add option to inverse state of relays

--- skarsta/lib/motor-relay/MotorRelay.cpp
+++ skarsta/lib/motor-relay/MotorRelay.cpp
@@ -2,22 +2,22 @@
 
 void MotorRelay::begin() {
     Motor::begin();
-    initPin(power_pin, HIGH);
-    initPin(dir_pin);
+    initPin(power_pin, DISABLE);
+    initPin(dir_pin, DISABLE);
 }
 
 void MotorRelay::_off() {
-    digitalWrite(power_pin, HIGH);
+    digitalWrite(power_pin, DISABLE);
 }
 
 void MotorRelay::_dir_cw() {
     _off();
-    digitalWrite(dir_pin, HIGH);
-    digitalWrite(power_pin, LOW);
+    digitalWrite(dir_pin, DISABLE);
+    digitalWrite(power_pin, ENABLE);
 }
 
 void MotorRelay::_dir_ccw() {
     _off();
-    digitalWrite(dir_pin, LOW);
-    digitalWrite(power_pin, LOW);
+    digitalWrite(dir_pin, ENABLE);
+    digitalWrite(power_pin, ENABLE);
 }
