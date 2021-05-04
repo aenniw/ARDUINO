b596912bb393db88cc89532a642782fb1148b7d4
Pascal <pascal.pothmann@gmail.com>
2021-05-04 12:56:48 +0200

    serial communication added

--- skarsta/lib/motor/Motor.h
+++ skarsta/lib/motor/Motor.h
@@ -88,6 +88,10 @@ public:
 
     MotorMode get_mode() const;
 
+    unsigned int get_end_stop_low();
+    unsigned int get_end_stop_high();
+    long get_next_position();
+
     void set_mode(MotorMode state);
 
     void disable() override;
